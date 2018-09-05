7efd2540848187ed4c789108e036269c2c08e752
william <grisa.william@gmail.com>
2018-09-05 16:09:08 -0300

    Add node cluster to server

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,26 @@
+import cluster from 'cluster'
+import os from 'os'
+
 import { app } from './app'
 import { config } from './config'
 import { logger } from './logger'
 
-app.listen(+config.http.port, config.http.host, () => {
-  logger.info(`Server started at [ http://${config.http.host}:${config.http.port} ]`)
-  logger.info(`Environment: ${config.env}`)
-})
+if (cluster.isMaster) {
+  logger.info(`Master ${process.pid} is running`)
+  const numCPUs = os.cpus().length
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  app.listen(+config.http.port, config.http.host, () => {
+    logger.info(`Server started at [ http://${config.http.host}:${config.http.port} ]`)
+    logger.info(`Environment ${process.pid}: ${config.env}`)
+  })
+
+  console.log(`Worker ${process.pid} started`)
+}
