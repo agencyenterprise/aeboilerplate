107db0c4663db4e55db646c6654f59f329b0934e
william <grisa.william@gmail.com>
2018-09-05 16:47:51 -0300

    Add no console rule and more logs to server

--- api/src/server.ts
+++ api/src/server.ts
@@ -9,18 +9,18 @@ if (cluster.isMaster) {
   logger.info(`Master ${process.pid} is running`)
   const numCPUs = os.cpus().length
 
+  logger.info(`Forking ${numCPUs} clusters`)
+
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork()
   }
 
   cluster.on('exit', (worker) => {
-    console.log(`worker ${worker.process.pid} died`)
+    logger.info(`worker ${worker.process.pid} died`)
   })
 } else {
   app.listen(+config.http.port, config.http.host, () => {
     logger.info(`Server started at [ http://${config.http.host}:${config.http.port} ]`)
     logger.info(`Environment ${process.pid}: ${config.env}`)
   })
-
-  console.log(`Worker ${process.pid} started`)
 }
