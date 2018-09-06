f98ff8fecb6a88e270aab3795c5a534aa90b0920
william <grisa.william@gmail.com>
2018-09-06 18:03:00 -0300

    Refactor client generator

--- client-generator/index.js
+++ client-generator/index.js
@@ -1,27 +1,67 @@
 #!/usr/bin/env node
 
-const resources = ['.editorconfig', '.gitignore', '.prettierrc', 'README.md', 'tsconfig.json', 'tslint.json']
+const shell = require('shelljs')
+const colors = require('colors')
+const fs = require('fs')
 
-let shell = require('shelljs')
-let colors = require('colors')
-let fs = require('fs')
+const resources = ['.editorconfig', '.gitignore', '.prettierrc', 'tsconfig.json', 'tslint.json']
+const clientGeneratorPath = `${process.cwd()}`
+const clientAppPath = '../client'
 
-let appDirectory = `${process.cwd()}/client`
+const run = async () => {
+  try {
+    await createReactApp()
+    await updateAppConfig()
+    await openAppFolder()
+    await installDependences()
+    console.log('\nAll done'.green)
+  } catch (error) {
+    console.log(error, 'Something went wrong while trying to create a new React app using create-react-app'.red)
+  }
+}
 
-const runCreateReactApp = () => {
+const createReactApp = () => {
   return new Promise((resolve) => {
-    console.log('\nRun create-react-app with TypeScript'.cyan)
-    shell.exec('create-react-app client --scripts-version=react-scripts-ts', () => {
-      console.log('\nCreated react app'.green)
+    console.log('\nRun create-react-app with TypeScript and SASS'.cyan)
+
+    const createReactAppCommand = `create-react-app ${clientAppPath} --scripts-version="@petejkim/react-scripts-ts-sass" --use-npm`
+
+    const onSuccess = () => {
+      console.log('\nReact app created'.green)
       resolve(true)
+    }
+
+    shell.exec(createReactAppCommand, onSuccess)
+  })
+}
+
+const updateAppConfig = () => {
+  return new Promise((resolve) => {
+    const configPromises = resources.map((resource) => copyResourceFile(resource))
+    Promise.all(configPromises).then(() => {
+      console.log('\nResources copied from generator to client'.cyan)
+      resolve()
     })
+  })
+}
+
+const copyResourceFile = (resource) => {
+  return new Promise((resolve) => {
+    const fromGeneratorResourcesPath = `${clientGeneratorPath}/resources/${resource}`
+    const toClientResourcesPath = `${clientAppPath}/${resource}`
+    const onErrorHandler = (error) => {
+      error && console.log(error.red)
+      resolve()
+    }
+
+    fs.copyFile(fromGeneratorResourcesPath, toClientResourcesPath, onErrorHandler)
   })
 }
 
 const openAppFolder = () => {
   return new Promise((resolve) => {
-    shell.cd('client')
-    console.log('\nMove into app directory'.cyan)
+    shell.cd(`${clientAppPath}`)
+    console.log('\nOpen client folder'.cyan)
     resolve(true)
   })
 }
@@ -31,46 +71,11 @@ const installDependences = () => {
     console.log('\nInstalling DEV dependences'.cyan)
     shell.exec('npm install -D prettier tslint tslint-config-prettier tslint-consistent-codestyle')
 
-    console.log('\nInstalling dependences'.cyan)
+    console.log('\nInstalling dependencies'.cyan)
     shell.exec(`npm install -S redux redux-thunk react-router-dom`)
 
-    shell.cd('..')
     resolve(true)
   })
-}
-
-const updateAppConfig = () => {
-  return new Promise((resolve) => {
-    const configPromises = []
-    resources.forEach((resource) => {
-      configPromises.push(createFile(resource))
-    })
-    Promise.all(configPromises).then(() => {
-      console.log('\nConfigurations updated'.cyan)
-      resolve()
-    })
-  })
-}
-
-const createFile = (resource) => {
-  return new Promise((resolve) => {
-    fs.copyFile(`client-generator/client-resources/${resource}`, `${appDirectory}/${resource}`, (error) => {
-      error && console.log(error.red)
-      resolve()
-    })
-  })
-}
-
-const run = async () => {
-  try {
-    await runCreateReactApp()
-    await openAppFolder()
-    await installDependences()
-    await updateAppConfig()
-    console.log('\nAll done'.green)
-  } catch (error) {
-    console.log(error, 'Something went wrong while trying to create a new React app using create-react-app'.red)
-  }
 }
 
 run()
