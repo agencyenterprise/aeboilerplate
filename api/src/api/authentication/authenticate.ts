ba78314a4d7e369d8796b06235c160222f29e379
william <grisa.william@gmail.com>
2018-09-10 18:16:09 -0300

    Reorder authenticate imports

--- api/src/api/authentication/authenticate.ts
+++ api/src/api/authentication/authenticate.ts
@@ -1,6 +1,6 @@
 import express from 'express'
-import passport from 'passport'
 import asyncHandler from 'express-async-handler'
+import passport from 'passport'
 
 import { authenticateCallback } from '../../middlewares/passport-callback-authenticator'
 
