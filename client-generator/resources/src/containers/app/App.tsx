e192b3de729500dcc15d714bab46c1a818a87cfc
Vanessa da Conceição <vanessa.sc22@gmail.com>
2018-09-12 14:45:17 -0300

    Fix encode

--- client-generator/resources/src/containers/app/App.tsx
+++ client-generator/resources/src/containers/app/App.tsx
@@ -1,14 +1,14 @@
-import * as React from ‘react’
+import * as React from 'react'
 
-import AuthenticationGateway from ‘../AuthenticationGateway’
-import ProtectedRoutes from ‘../ProtectedRoutes’
+import AuthenticationGateway from '../AuthenticationGateway'
+import ProtectedRoutes from '../ProtectedRoutes'
 
 const App = ({ location }: { location: any }) => {
- return (
-   <div>
-     <AuthenticationGateway component={ProtectedRoutes} location={location} path=“/” />
-   </div>
- )
+  return (
+    <div>
+      <AuthenticationGateway component={ProtectedRoutes} location={location} path="/" />
+    </div>
+  )
 }
 
 export default App
