7b56866882623fe68a0b8123bb5429cea748834a
william <grisa.william@gmail.com>
2018-09-27 17:55:46 -0300

    Remove unnecessary code

--- api/src/api/me/get-me.ts
+++ api/src/api/me/get-me.ts
@@ -3,9 +3,5 @@ import { getMe } from '../../services/me/get-me'
 export const fetchMe = async (req, res) => {
   const user = await getMe(req)
 
-  if (!user) {
-    return res.sendStatus(401)
-  }
-
   return res.status(200).send(user)
 }
