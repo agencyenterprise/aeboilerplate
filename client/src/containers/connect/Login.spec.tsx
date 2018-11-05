48ddd0852a03990325f376dd4625a9b9d1e13ede
william <grisa.william@gmail.com>
2018-11-05 12:57:03 -0200

    Use jest snapshots to test for rendered login

--- client/src/containers/connect/Login.spec.tsx
+++ client/src/containers/connect/Login.spec.tsx
@@ -1,6 +1,7 @@
 import { configure, shallow } from 'enzyme'
 import * as Adapter from 'enzyme-adapter-react-16'
 import * as React from 'react'
+import * as renderer from 'react-test-renderer'
 
 import { config } from '../../config'
 import { Login } from './Login'
@@ -8,8 +9,9 @@ import { Login } from './Login'
 configure({ adapter: new Adapter() })
 
 describe(`<Login />`, () => {
-  it('renders without throwing an error', () => {
-    expect(shallow(<Login />).exists()).toBeTruthy()
+  it('renders', () => {
+    const login = renderer.create(<Login />).toJSON()
+    expect(login).toMatchSnapshot()
   })
 
   it('has login class', () => {
